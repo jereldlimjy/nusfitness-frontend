d7e2f38637912841b8e70ca76eeb00f912b0a8d4
Tan Jia Xian <e0543981@u.nus.edu>
2021-05-31 16:05:24 +0800

    added date and slots left

--- src/components/Slot.js
+++ src/components/Slot.js
@@ -1,5 +1,10 @@
-const Slot = ({ hour }) => {
-  return <button className="btn">{hour}</button>;
+const Slot = ({ hour, slotsLeft }) => {
+  return (
+    <div className="card all-center">
+      <button className="btn">{hour}</button>
+      <i>{`Slots left: ${slotsLeft}`}</i>
+    </div>
+  );
 };
 
 export default Slot;
--- src/components/SlotContainer.js
+++ src/components/SlotContainer.js
@@ -1,10 +1,15 @@
 import Slot from "./Slot";
 
-const SlotContainer = ({ hours }) => {
+const SlotContainer = ({ date, hours }) => {
   return (
     <div className="card all-center">
+      <h2 className="lead">{date}</h2>
+
       {hours.map((hourObject) => (
-        <Slot hour={hourObject.active && hourObject.time} />
+        <Slot
+          hour={hourObject.active && hourObject.time}
+          slotsLeft={hourObject.slotsLeft}
+        />
       ))}
     </div>
   );
