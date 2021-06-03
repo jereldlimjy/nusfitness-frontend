9e0c277fc1b1395a80badf5d57352b83ddbe6d59
Tan Jia Xian <e0543981@u.nus.edu>
2021-06-03 23:03:41 +0800

    facility dropdown

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,167 +1,201 @@
+import { useState } from "react";
 import SlotContainer from "./SlotContainer";
 
 const Booking = () => {
-  const allFacilitiesHours = {
+  const facilityHours = {
     poolKrWeekday: [
-      { time: "0730", active: true, slotsLeft: 40 },
-      { time: "0900", active: true, slotsLeft: 40 },
-      { time: "1000", active: true, slotsLeft: 40 },
-      { time: "1100", active: true, slotsLeft: 40 },
-      { time: "1200", active: true, slotsLeft: 40 },
-      { time: "1300", active: true, slotsLeft: 40 },
-      { time: "1400", active: true, slotsLeft: 40 },
-      { time: "1500", active: true, slotsLeft: 40 },
-      { time: "1600", active: true, slotsLeft: 40 },
-      { time: "1700", active: true, slotsLeft: 40 },
-      { time: "1800", active: true, slotsLeft: 40 },
-      { time: "1900", active: true, slotsLeft: 40 },
-      { time: "2000", active: true, slotsLeft: 40 },
+      "0730",
+      "0900",
+      "1000",
+      "1100",
+      "1200",
+      "1300",
+      "1400",
+      "1500",
+      "1600",
+      "1700",
+      "1800",
+      "1900",
+      "2000",
     ],
     poolKrWeekend: [
-      { time: "0900", active: true, slotsLeft: 40 },
-      { time: "1000", active: true, slotsLeft: 40 },
-      { time: "1100", active: true, slotsLeft: 40 },
-      { time: "1200", active: true, slotsLeft: 40 },
-      { time: "1300", active: true, slotsLeft: 40 },
-      { time: "1400", active: true, slotsLeft: 40 },
-      { time: "1500", active: true, slotsLeft: 40 },
-      { time: "1600", active: true, slotsLeft: 40 },
-      { time: "1700", active: true, slotsLeft: 40 },
-      { time: "1800", active: true, slotsLeft: 40 },
+      "0900",
+      "1000",
+      "1100",
+      "1200",
+      "1300",
+      "1400",
+      "1500",
+      "1600",
+      "1700",
+      "1800",
     ],
     poolUtownWeekday: [
-      { time: "0730", active: true, slotsLeft: 40 },
-      { time: "0900", active: true, slotsLeft: 40 },
-      { time: "1000", active: true, slotsLeft: 40 },
-      { time: "1100", active: true, slotsLeft: 40 },
-      { time: "1200", active: true, slotsLeft: 40 },
-      { time: "1300", active: true, slotsLeft: 40 },
-      { time: "1400", active: true, slotsLeft: 40 },
-      { time: "1500", active: true, slotsLeft: 40 },
-      { time: "1600", active: true, slotsLeft: 40 },
-      { time: "1700", active: true, slotsLeft: 40 },
-      { time: "1800", active: true, slotsLeft: 40 },
-      { time: "1900", active: true, slotsLeft: 40 },
-      { time: "2000", active: true, slotsLeft: 40 },
+      "0730",
+      "0900",
+      "1000",
+      "1100",
+      "1200",
+      "1300",
+      "1400",
+      "1500",
+      "1600",
+      "1700",
+      "1800",
+      "1900",
+      "2000",
     ],
     poolUtownWeekend: [
-      { time: "0900", active: true, slotsLeft: 40 },
-      { time: "1000", active: true, slotsLeft: 40 },
-      { time: "1100", active: true, slotsLeft: 40 },
-      { time: "1200", active: true, slotsLeft: 40 },
-      { time: "1300", active: true, slotsLeft: 40 },
-      { time: "1400", active: true, slotsLeft: 40 },
-      { time: "1500", active: true, slotsLeft: 40 },
-      { time: "1600", active: true, slotsLeft: 40 },
-      { time: "1700", active: true, slotsLeft: 40 },
-      { time: "1800", active: true, slotsLeft: 40 },
+      "0900",
+      "1000",
+      "1100",
+      "1200",
+      "1300",
+      "1400",
+      "1500",
+      "1600",
+      "1700",
+      "1800",
     ],
     gymKrWeekday: [
-      { time: "1100", active: true, slotsLeft: 40 },
-      { time: "1200", active: true, slotsLeft: 40 },
-      { time: "1300", active: true, slotsLeft: 40 },
-      { time: "1400", active: true, slotsLeft: 40 },
-      { time: "1500", active: true, slotsLeft: 40 },
-      { time: "1600", active: true, slotsLeft: 40 },
-      { time: "1700", active: true, slotsLeft: 40 },
-      { time: "1800", active: true, slotsLeft: 40 },
-      { time: "1900", active: true, slotsLeft: 40 },
+      "1100",
+      "1200",
+      "1300",
+      "1400",
+      "1500",
+      "1600",
+      "1700",
+      "1800",
+      "1900",
     ],
     gymKrWeekend: [],
     gymUscWeekday: [
-      { time: "0900", active: true, slotsLeft: 40 },
-      { time: "1000", active: true, slotsLeft: 40 },
-      { time: "1100", active: true, slotsLeft: 40 },
-      { time: "1200", active: true, slotsLeft: 40 },
-      { time: "1300", active: true, slotsLeft: 40 },
-      { time: "1400", active: true, slotsLeft: 40 },
-      { time: "1500", active: true, slotsLeft: 40 },
-      { time: "1600", active: true, slotsLeft: 40 },
-      { time: "1700", active: true, slotsLeft: 40 },
-      { time: "1800", active: true, slotsLeft: 40 },
-      { time: "1900", active: true, slotsLeft: 40 },
-      { time: "2000", active: true, slotsLeft: 40 },
+      "0900",
+      "1000",
+      "1100",
+      "1200",
+      "1300",
+      "1400",
+      "1500",
+      "1600",
+      "1700",
+      "1800",
+      "1900",
+      "2000",
     ],
     gymUscWeekend: [
-      { time: "0900", active: true, slotsLeft: 40 },
-      { time: "1000", active: true, slotsLeft: 40 },
-      { time: "1100", active: true, slotsLeft: 40 },
-      { time: "1200", active: true, slotsLeft: 40 },
-      { time: "1300", active: true, slotsLeft: 40 },
-      { time: "1400", active: true, slotsLeft: 40 },
-      { time: "1500", active: true, slotsLeft: 40 },
-      { time: "1600", active: true, slotsLeft: 40 },
-      { time: "1700", active: true, slotsLeft: 40 },
-      { time: "1800", active: true, slotsLeft: 40 },
+      "0900",
+      "1000",
+      "1100",
+      "1200",
+      "1300",
+      "1400",
+      "1500",
+      "1600",
+      "1700",
+      "1800",
     ],
     gymUtownWeekday: [
-      { time: "0700", active: true, slotsLeft: 40 },
-      { time: "0800", active: true, slotsLeft: 40 },
-      { time: "0900", active: true, slotsLeft: 40 },
-      { time: "1000", active: true, slotsLeft: 40 },
-      { time: "1100", active: true, slotsLeft: 40 },
-      { time: "1200", active: true, slotsLeft: 40 },
-      { time: "1300", active: true, slotsLeft: 40 },
-      { time: "1400", active: true, slotsLeft: 40 },
-      { time: "1500", active: true, slotsLeft: 40 },
-      { time: "1600", active: true, slotsLeft: 40 },
-      { time: "1700", active: true, slotsLeft: 40 },
-      { time: "1800", active: true, slotsLeft: 40 },
-      { time: "1900", active: true, slotsLeft: 40 },
-      { time: "2000", active: true, slotsLeft: 40 },
-      { time: "2100", active: true, slotsLeft: 40 },
+      "0700",
+      "0800",
+      "0900",
+      "1000",
+      "1100",
+      "1200",
+      "1300",
+      "1400",
+      "1500",
+      "1600",
+      "1700",
+      "1800",
+      "1900",
+      "2000",
+      "2100",
     ],
     gymUtownWeekend: [
-      { time: "0700", active: true, slotsLeft: 40 },
-      { time: "0800", active: true, slotsLeft: 40 },
-      { time: "0900", active: true, slotsLeft: 40 },
-      { time: "1000", active: true, slotsLeft: 40 },
-      { time: "1100", active: true, slotsLeft: 40 },
-      { time: "1200", active: true, slotsLeft: 40 },
-      { time: "1300", active: true, slotsLeft: 40 },
-      { time: "1400", active: true, slotsLeft: 40 },
-      { time: "1500", active: true, slotsLeft: 40 },
-      { time: "1600", active: true, slotsLeft: 40 },
-      { time: "1700", active: true, slotsLeft: 40 },
-      { time: "1800", active: true, slotsLeft: 40 },
-      { time: "1900", active: true, slotsLeft: 40 },
-      { time: "2000", active: true, slotsLeft: 40 },
-      { time: "2100", active: true, slotsLeft: 40 },
+      "0700",
+      "0800",
+      "0900",
+      "1000",
+      "1100",
+      "1200",
+      "1300",
+      "1400",
+      "1500",
+      "1600",
+      "1700",
+      "1800",
+      "1900",
+      "2000",
+      "2100",
     ],
     gymWellnessWeekday: [
-      { time: "0700", active: true, slotsLeft: 40 },
-      { time: "0800", active: true, slotsLeft: 40 },
-      { time: "0900", active: true, slotsLeft: 40 },
-      { time: "1000", active: true, slotsLeft: 40 },
-      { time: "1100", active: true, slotsLeft: 40 },
-      { time: "1200", active: true, slotsLeft: 40 },
-      { time: "1300", active: true, slotsLeft: 40 },
-      { time: "1400", active: true, slotsLeft: 40 },
-      { time: "1500", active: true, slotsLeft: 40 },
-      { time: "1600", active: true, slotsLeft: 40 },
-      { time: "1700", active: true, slotsLeft: 40 },
-      { time: "1800", active: true, slotsLeft: 40 },
-      { time: "1900", active: true, slotsLeft: 40 },
-      { time: "2000", active: true, slotsLeft: 40 },
-      { time: "2100", active: true, slotsLeft: 40 },
+      "0700",
+      "0800",
+      "0900",
+      "1000",
+      "1100",
+      "1200",
+      "1300",
+      "1400",
+      "1500",
+      "1600",
+      "1700",
+      "1800",
+      "1900",
+      "2000",
+      "2100",
     ],
     gymWellnessWeekend: [],
   };
 
+  const dates = ["3 May 2021", "4 May 2021", "5 May 2021"];
+
+  const [selectedSlots, setSelectedSlots] = useState([]);
+
+  const handleSlotChange = (e) => {
+    console.log(e);
+    // name, facility, timeBooked, date
+    // this.setSelectedSlots([...selectedSlots, ])
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+    console.log(e.target);
+  };
+
+  const [selectedHours, setSelectedHours] = useState(
+    facilityHours.poolUtownWeekday
+  );
+  const handleFacilityChange = (e) => {
+    // console.log(e.target.value);
+    // console.log(Object.entries(facilityHours)[e.target.value * 2][1]);
+    setSelectedHours(Object.entries(facilityHours)[e.target.value * 2][1]);
+    console.log(selectedHours);
+  };
+
   return (
-    <div className="container-vert">
-      <SlotContainer
-        date="3 May 2021"
-        hours={allFacilitiesHours.gymUtownWeekday}
-      />
-      <SlotContainer
-        date="4 May 2021"
-        hours={allFacilitiesHours.gymUtownWeekday}
-      />
-      <SlotContainer
-        date="5 May 2021"
-        hours={allFacilitiesHours.gymUtownWeekday}
-      />
+    <div>
+      <label htmlFor="facility">Select facility:</label>
+      <select name="facility" id="facility" onChange={handleFacilityChange}>
+        <option value={0}>Kent Ridge Swimming Pool</option>
+        <option value={1}>University Town Swimming Pool</option>
+        <option value={2}>Kent Ridge Gym</option>
+        <option value={3}>University Sports Centre Gym</option>
+        <option value={4}>University Town Gym</option>
+        <option value={5}>Wellness Outreach Gym</option>
+      </select>
+
+      <form onSubmit={handleSubmit} className="container-vert">
+        <SlotContainer
+          date={dates[0]} // TODO: date generated within SlotContainer
+          hours={selectedHours}
+          handleChange={handleSlotChange}
+        />
+        <SlotContainer date={dates[1]} hours={selectedHours} />
+        <SlotContainer date={dates[2]} hours={selectedHours} />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 };
--- src/components/SlotContainer.js
+++ src/components/SlotContainer.js
@@ -1,16 +1,12 @@
 import Slot from "./Slot";
 
-const SlotContainer = ({ date, hours }) => {
+const SlotContainer = ({ date, hours, handleChange }) => {
   return (
     <div className="card all-center">
       <h2 className="lead">{date}</h2>
 
-      {hours.map((hourObject) => (
-        <Slot
-          date={date}
-          hour={hourObject.active && hourObject.time}
-          slotsLeft={hourObject.slotsLeft}
-        />
+      {hours.map((hour) => (
+        <Slot date={date} hour={hour} handleChange={handleChange} />
       ))}
     </div>
   );
